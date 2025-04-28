1f4d2ffe95a8ac6a7f3cee28a9f08ff8d1f0e50a
MUGENI Giramata Aliane <amugenigiramata@gmail.com>
2025-04-28 19:45:56 +0200

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+
 import { FaSun, FaMoon } from "react-icons/fa";
 import { useTheme } from "./ThemeProvider"; 
 
@@ -5,12 +6,12 @@ function Header() {
   const { theme, toggleDarkMode } = useTheme(); 
 
   return (
-    <header className="flex justify-between items-center mb-8">
+    <header className="flex justify-between items-center mb-6">
       <h1 className="text-2xl font-bold text-black dark:text-white">devfinder</h1>
 
       <button
         onClick={toggleDarkMode}
-        className="flex items-center gap-2 text-sm font-bold tracking-widest text-gray-600 dark:text-white uppercase hover:text-blue-500 transition-colors"
+        className="flex items-center gap-2 text-sm font-bold  text-gray-600 dark:text-white uppercase hover:text-blue-500 transition-colors"
       >
         {theme === "dark" ? (
           <div>
